e9dc956995b1d0d3e133fd26f8aa298bd167526c
Raoni Alves Rocha <raoniguimao@gmail.com>
2024-08-21 11:41:57 -0300

    Troca busca para API

--- src/app/page.js
+++ src/app/page.js
@@ -3,16 +3,42 @@ import { Categorias } from "./components/Categorias";
 import { Produtos } from "./components/Produtos";
 import { getCategorias, getTodosProdutos } from "../lib/api";
 
-export default async function Home() {
-  const produtos = getTodosProdutos();
-  const categorias = getCategorias();
+async function fetchProdutosApi() {
+	const res = await fetch("http://localhost:3000/api/produtos");
 
-  return (
-    <>
-      <main className={styles.main}>
-        <Categorias categorias={categorias} />
-        <Produtos produtos={produtos} />
-      </main>
-    </>
-  );
+	if (!res.ok) {
+		throw new Error("Não foi possível obter os dados.");
+	}
+
+	const produtos = await res.json();
+
+	return produtos;
+}
+
+async function fetchCategoriasApi() {
+	const res = await fetch("http://localhost:3000/api/categorias");
+
+	if (!res.ok) {
+		throw new Error("Não foi possível obter os dados.");
+	}
+
+	const categorias = await res.json();
+
+	return categorias;
+}
+
+export default async function Home() {
+	// const produtos = getTodosProdutos();
+	// const categorias = getCategorias();
+	const { produtos } = await fetchProdutosApi();
+	const { categorias } = await fetchCategoriasApi();
+
+	return (
+		<>
+			<main className={styles.main}>
+				<Categorias categorias={categorias} />
+				<Produtos produtos={produtos} />
+			</main>
+		</>
+	);
 }
